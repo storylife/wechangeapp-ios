78da3c34e3da3604ec1b6a270cb4d4cb95f6f95b
Martin <tenstar718@gmail.com>
2021-07-15 20:50:05 +0800

    Initial Commit

---
+++ wechangeapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wechangeapp
+//
+//  Created by Shadow on 2021/7/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
