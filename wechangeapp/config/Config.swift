5e87d3862d440036a4c3d4fa06bfe1160d90d786
Oliver Brzoska <oliver@goodsquirrel.com>
2021-08-12 09:49:55 +0200

    Update App title, icon and URLs for customer 'Scientists for Future SKIP'

--- wechangeapp/config/Config.swift
+++ wechangeapp/config/Config.swift
@@ -6,36 +6,79 @@
 //
 
 import Foundation
-class Config {
-    static var WECHANGE_URL: String = "https://wechange.de/dashboard/"
-    static var NOTIFICATION_URL: String = "https://wechange.de/profile/api/alerts/get/"
-    static var MARKSEEN_URL: String = "https://wechange.de/profile/api/alerts/markseen/"
-    static var DASHBOARD_URL: String = "https://wechange.de/dashboard/"
-    static var WECHANGE_MESSAGES_URL: String = "https://wechange.de/messages/"
-    static var WECHANGE_ROCKET_CHAT_URL: String = "https://rocketchat.wechange.de"
+enum Config {
     
-    static var APP_ROCKET_CHAT: String = "chat.rocket.android"
+    // customer specific constants
+    static let WECHANGE_URL: String = "https://skip.scientists4future.org/dashboard/"
+    static let NOTIFICATION_URL: String = "https://skip.scientists4future.org/profile/api/alerts/get/"
+    static let MARKSEEN_URL: String = "https://skip.scientists4future.org/profile/api/alerts/markseen/"
+    static let DASHBOARD_URL: String = "https://skip.scientists4future.org/dashboard/"
+    static let WECHANGE_MESSAGES_URL: String = "https://skip.scientists4future.org/messages/"
+    static let WECHANGE_ROCKET_CHAT_URL: String = "https://chat.skip.scientists4future.org"
+    static let WECHANGE_NEXTCLOUD_URL: String = "https://cloud.skip.scientists4future.org"
     
-    static var NOTIFICATIONS_REFRESH_INTERVAL: Double = 10 * 60 * 1000
-    static var CHANNEL_PLATFORM_NOTIFICATIONS_ID: String = "platform_notifications"
-    static var REQUEST_TIMER_EVERY_TEN_MINUTES: Int = 1
     
-    static var INTENT_KEY_URL: String = "url"
-    static var INTENT_KEY_TIMESTAMP: String = "timestampToMarkAsSeen"
+    // generic constants
+    static let APP_ROCKET_CHAT: String = "chat.rocket.android"
     
-    static var JSON_KEY_DATA: String = "data"
-    static var JSON_KEY_NEWEST_TIMESTAMP: String = "newest_timestamp"
-    static var JSON_KEY_ITEMS: String = "items"
-    static var JSON_KEY_IS_EMPHASIZED: String = "is_emphasized"
-    static var JSON_KEY_TEXT: String = "text"
-    static var JSON_KEY_GROUP: String = "group"
-    static var JSON_KEY_ID: String = "id"
-    static var JSON_KEY_URL: String = "url"
+    static let NOTIFICATIONS_REFRESH_INTERVAL: Double = 10 * 60 * 1000
+    static let CHANNEL_PLATFORM_NOTIFICATIONS_ID: String = "platform_notifications"
+    static let REQUEST_TIMER_EVERY_TEN_MINUTES: Int = 1
     
-    static var HTTP_HEADER_COOKIE: String = "Cookie"
-    static var HTTP_HEADER_CSFR_TOKEN: String = "X-CSRFToken"
-    static var HTTP_HEADER_REFERER: String = "Referer"
+    static let INTENT_KEY_URL: String = "url"
+    static let INTENT_KEY_TIMESTAMP: String = "timestampToMarkAsSeen"
     
-    static var TAG_EXTERNAL_APP_INSTALLED: String = "external-app-installed"
-    static var TAG_EXTERNAL_APP_NOT_INSTALLED: String = "external-app-not-installed"
+    static let JSON_KEY_DATA: String = "data"
+    static let JSON_KEY_NEWEST_TIMESTAMP: String = "newest_timestamp"
+    static let JSON_KEY_ITEMS: String = "items"
+    static let JSON_KEY_IS_EMPHASIZED: String = "is_emphasized"
+    static let JSON_KEY_TEXT: String = "text"
+    static let JSON_KEY_GROUP: String = "group"
+    static let JSON_KEY_ID: String = "id"
+    static let JSON_KEY_URL: String = "url"
+    
+    static let HTTP_HEADER_COOKIE: String = "Cookie"
+    static let HTTP_HEADER_CSFR_TOKEN: String = "X-CSRFToken"
+    static let HTTP_HEADER_REFERER: String = "Referer"
+    
+    static let TAG_EXTERNAL_APP_INSTALLED: String = "external-app-installed"
+    static let TAG_EXTERNAL_APP_NOT_INSTALLED: String = "external-app-not-installed"
 }
+
+// WeChange default config
+
+//enum Config {
+//
+//    static let WECHANGE_URL: String = "https://wechange.de/dashboard/"
+//    static let NOTIFICATION_URL: String = "https://wechange.de/profile/api/alerts/get/"
+//    static let MARKSEEN_URL: String = "https://wechange.de/profile/api/alerts/markseen/"
+//    static let DASHBOARD_URL: String = "https://wechange.de/dashboard/"
+//    static let WECHANGE_MESSAGES_URL: String = "https://wechange.de/messages/"
+//    static let WECHANGE_ROCKET_CHAT_URL: String = "https://rocketchat.wechange.de"
+//
+//    static let APP_ROCKET_CHAT: String = "chat.rocket.android"
+//
+//    static let NOTIFICATIONS_REFRESH_INTERVAL: Double = 10 * 60 * 1000
+//    static let CHANNEL_PLATFORM_NOTIFICATIONS_ID: String = "platform_notifications"
+//    static let REQUEST_TIMER_EVERY_TEN_MINUTES: Int = 1
+//
+//    static let INTENT_KEY_URL: String = "url"
+//    static let INTENT_KEY_TIMESTAMP: String = "timestampToMarkAsSeen"
+//
+//    static let JSON_KEY_DATA: String = "data"
+//    static let JSON_KEY_NEWEST_TIMESTAMP: String = "newest_timestamp"
+//    static let JSON_KEY_ITEMS: String = "items"
+//    static let JSON_KEY_IS_EMPHASIZED: String = "is_emphasized"
+//    static let JSON_KEY_TEXT: String = "text"
+//    static let JSON_KEY_GROUP: String = "group"
+//    static let JSON_KEY_ID: String = "id"
+//    static let JSON_KEY_URL: String = "url"
+//
+//    static let HTTP_HEADER_COOKIE: String = "Cookie"
+//    static let HTTP_HEADER_CSFR_TOKEN: String = "X-CSRFToken"
+//    static let HTTP_HEADER_REFERER: String = "Referer"
+//
+//    static let TAG_EXTERNAL_APP_INSTALLED: String = "external-app-installed"
+//    static let TAG_EXTERNAL_APP_NOT_INSTALLED: String = "external-app-not-installed"
+//}
+    
